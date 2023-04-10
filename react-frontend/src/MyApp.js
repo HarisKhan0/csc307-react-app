282586cb5afcf7ef26194a6e00869b2bdb237012
HarisKhan0 <71156152+HarisKhan0@users.noreply.github.com>
2023-04-10 08:38:47 -0700

    Changed for input on site

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,33 +1,19 @@
-
-
+import Form from './Form';
 import React, {useState} from 'react';
 import Table from './Table';
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Haris',
-        job: 'Student'
-      },
-      {
-        name: 'BJ',
-        job: 'Professor'
-      },
-      {
-        name: 'Winton',
-        job: 'Student'
-      }
-    ]
-  );  
+  const [characters, setCharacters] = useState([]);
+
+
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form />
     </div>
   )
+  
+  
 function removeOneCharacter (index) {
   const updated = characters.filter((character, i) => {
       return i !== index
